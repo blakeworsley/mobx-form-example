60d2e26d38f4f57c0a57c48590c03b0c15eb8781
Blake Worsley <blakeworsley@gmail.com>
2017-11-15 17:13:16 -0700

    adds babel rc

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react'
 import '../styles/App.css';
 
+// @observer
 export default class App extends Component {
 
   render() {
+    console.log(observer);
     return (
       <div>
         App
--- src/components/GlobalApp.js
+++ src/components/GlobalApp.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { autorun } from 'mobx';
-import { observer, Provider} from 'mobx-react';
+import { Provider } from 'mobx-react';
 import RootStore from '../stores/root';
 import App from './App';
 
-@observer
 export default class GlobalApp extends Component {
   constructor(props) {
     super(props);
